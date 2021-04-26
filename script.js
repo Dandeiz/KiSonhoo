e8d245ce31f3c124b22aeba4c998c73bc05b7ea2
Dandeiz <daniloflorenzano1@outlook.com>
2021-04-26 13:49:35 -0300

    svg configurados

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Scroll suave 
+
 jQuery(document).ready(function($) { 
     $(".scroll").click(function(event){        
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
    });
 });
+
